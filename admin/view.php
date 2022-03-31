d7540e1ec86ae85949799595305d4a80147fc00f
David Gilson <davgilson@live.fr>
2022-03-31 21:45:37 +0200

    add login

---
+++ admin/view.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php $title="Admin"; require "../head.php"; ?>
+<body>
+    <?php require "../header.php"; ?>
+    <h1>You are connected !!!</h1>
+    <?php require "../footer.php"; ?>
+</body>
+</html>
