37a794704df988ab124fac24b54b20f5c99c8d92
David Gilson <davgilson@live.fr>
2022-05-19 21:09:47 +0200

    add joke

--- head.php
+++ head.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $appName ?><?= isset($title) ? " - $title" : '' ?></title>
+    <link rel="icon" type="image/x-icon" href="/bioblog/favicon.ico" />
     <meta name="description" content="<?= $site_description ?>" />
 
     <meta property="og:type" content="article" />
---
+++ joke/model.php
@@ -0,0 +1,9 @@
+<?php
+
+$jokes = [
+    'Check Norris a déjà compté jusqu\'à l\'infini. Deux fois.',
+    'Certaines personnes portent un pyjama Superman. Superman porte un pyjama Chuck Norris.',
+    'Chuck Norris peut serrer la main droite de Jamel',
+    'Chuck Norris ne se mouille pas, c\'est l\'eau qui se Chuck Norris',
+    'Chuck Norris a terminé l\'histoire sans fin.'
+];
